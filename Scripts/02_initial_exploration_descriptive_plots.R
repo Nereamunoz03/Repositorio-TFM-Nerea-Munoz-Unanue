627e042862ca54217dbe1e20cee9db509f76cc9d
Nereamunoz03 <munoznerea03@gmail.com>
2026-07-06 20:35:15 +0200

    Update 02_initial_exploration_descriptive_plots.R

--- Scripts/02_initial_exploration_descriptive_plots.R
+++ Scripts/02_initial_exploration_descriptive_plots.R
@@ -2,8 +2,8 @@
 # Trabajo Fin de Máster
 # Máster Universitario en Bioinformática - UNIR
 #
-# Título: Análisis del splicing alternativo asociado a la progresión tumoral
-#          en carcinoma renal de células claras mediante RNA-seq
+# Título: Análisis multiómico de expresión génica y splicing alternativo en 
+#         carcinoma renal de células claras (KIRC).
 #
 # Script: 02_initial_exploration_descriptive_plots.R
 # Correspondiente al apartado: 3.2.1. Análisis exploratorio inicial y
