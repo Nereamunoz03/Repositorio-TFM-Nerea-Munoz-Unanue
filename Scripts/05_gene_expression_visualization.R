f7ff936cea7e048ae65ebad9fb7c16464ecaf5c8
Nereamunoz03 <munoznerea03@gmail.com>
2026-07-06 20:35:45 +0200

    Update 05_gene_expression_visualization.R

--- Scripts/05_gene_expression_visualization.R
+++ Scripts/05_gene_expression_visualization.R
@@ -2,8 +2,8 @@
 # Trabajo Fin de Máster
 # Máster Universitario en Bioinformática - UNIR
 #
-# Título: Análisis del splicing alternativo asociado a la progresión tumoral
-#          en carcinoma renal de células claras mediante RNA-seq
+# Título: Análisis multiómico de expresión génica y splicing alternativo en 
+#         carcinoma renal de células claras (KIRC).
 #
 # Script: 05_gene_expression_visualization.R
 # Correspondiente al apartado: 3.3.4. Visualización de los perfiles de
