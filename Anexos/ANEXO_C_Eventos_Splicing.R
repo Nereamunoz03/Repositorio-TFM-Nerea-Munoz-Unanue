13724ed8a064a59fc369f1fbf24c926efa96a34e
Nereamunoz03 <munoznerea03@gmail.com>
2026-07-06 20:34:39 +0200

    Update ANEXO_C_Eventos_Splicing.R

--- Anexos/ANEXO_C_Eventos_Splicing.R
+++ Anexos/ANEXO_C_Eventos_Splicing.R
@@ -2,8 +2,8 @@
 # Trabajo Fin de Máster
 # Máster Universitario en Bioinformática - UNIR
 #
-# Título: Análisis del splicing alternativo asociado a la progresión tumoral
-#          en carcinoma renal de células claras mediante RNA-seq
+# Título: Análisis multiómico de expresión génica y splicing alternativo en 
+#         carcinoma renal de células claras (KIRC).
 #
 # Script: ANEXO_C_Eventos_Splicing.R
 # Correspondiente al Anexo C del repositorio.
