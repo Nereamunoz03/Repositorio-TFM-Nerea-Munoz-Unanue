23da3dd8a31877c783099b486309f8df5cb05149
Nereamunoz03 <munoznerea03@gmail.com>
2026-07-06 20:36:55 +0200

    Update 07_differential_splicing_visualization.R

--- Scripts/07_differential_splicing_visualization.R
+++ Scripts/07_differential_splicing_visualization.R
@@ -2,8 +2,8 @@
 # Trabajo Fin de Máster
 # Máster Universitario en Bioinformática - UNIR
 #
-# Título: Análisis del splicing alternativo asociado a la progresión tumoral
-#          en carcinoma renal de células claras mediante RNA-seq
+# Título: Análisis multiómico de expresión génica y splicing alternativo en 
+#         carcinoma renal de células claras (KIRC).
 #
 # Script: 07_differential_splicing_visualization.R
 # Correspondiente al apartado: 3.4.2. Visualización de los eventos candidatos.
